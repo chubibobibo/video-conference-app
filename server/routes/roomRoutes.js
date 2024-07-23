5acf3386324ffeb718a22d144faf3ad0efbc1e3d
Alistair Abao <abaolester@gmail.com>
2024-07-23 16:04:11 +0200

    peer joining the same room

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import { addRoom, findRoom } from "../controllers/roomControllers.js";
 
-router.post("/addRoom", addRoom);
 router.get("/findRoom/:id", findRoom);
+router.post("/addRoom", addRoom);
 
 export default router;
