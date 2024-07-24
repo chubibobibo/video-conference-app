1a2a3287b4bf7fb290a817b56e32f404e5dc5342
Alistair Abao <abaolester@gmail.com>
2024-07-24 12:04:26 +0200

    protected routes using middleware

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
+import { isLoggedIn } from "../middleware/isLoggedIn.js";
+
 import { addRoom, findRoom } from "../controllers/roomControllers.js";
 
-router.get("/findRoom/:id", findRoom);
+router.get("/findRoom/:id", isLoggedIn, findRoom);
 router.post("/addRoom", addRoom);
 
 export default router;
